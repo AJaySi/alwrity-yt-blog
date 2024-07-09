370041817012cc4361d2aca951ed626233bf0c12
ajaysi <ajay.calsoft@gmail.com>
2024-07-09 19:43:20 +0530

    Improve user input, prompts & output

--- alwrity_yt_blog.py
+++ alwrity_yt_blog.py
@@ -5,6 +5,30 @@ from openai import OpenAI
 import streamlit as st
 from pytube import YouTube
 from tenacity import retry, stop_after_attempt, wait_random_exponential
+import google.generativeai as genai
+from youtube_transcript_api import YouTubeTranscriptApi
+import re
+
+
+def get_youtube_transcript(yt_url):
+	"""
+	Fetches the transcript of a YouTube video given its URL.
+	
+	Args:
+	    yt_url (str): The URL of the YouTube video.
+	
+	Returns:
+	    list: A list of dictionaries representing the transcript segments,
+	          or None if the transcript is not available.
+	"""
+	video_id = re.search(r'v=([^&]+)', yt_url).group(1)
+	try:
+	    transcript = YouTubeTranscriptApi.get_transcript(video_id)
+	    return transcript
+	except:
+	    print("Error: Transcript not available for this video.")
+	    return None
+
 
 def main():
     set_page_config()
@@ -15,7 +39,7 @@ def main():
 
 def set_page_config():
     st.set_page_config(
-        page_title="Alwrity - YouTube to Blog Generator",
+        page_title="Alwrity - AI YouTube to Blog Generator",
         layout="wide",
     )
 
@@ -28,23 +52,39 @@ def custom_css():
                 padding-left: 1rem;
                 padding-right: 1rem;
             }
-        </style>
-    """, unsafe_allow_html=True)
+                    ::-webkit-scrollbar-track {
+        background: #e1ebf9;
+        }
 
-    st.markdown("""
-        <style>
-            [class="st-emotion-cache-7ym5gk ef3psqc12"] {
-                display: inline-block;
-                padding: 5px 20px;
-                background-color: #4681f4;
-                color: #FBFFFF;
-                width: 300px;
-                height: 35px;
-                text-align: center;
-                text-decoration: none;
-                font-size: 16px;
-                border-radius: 8px;
-            }
+        ::-webkit-scrollbar-thumb {
+            background-color: #90CAF9;
+            border-radius: 10px;
+            border: 3px solid #e1ebf9;
+        }
+
+        ::-webkit-scrollbar-thumb:hover {
+            background: #64B5F6;
+        }
+
+        ::-webkit-scrollbar {
+            width: 16px;
+        }
+        div.stButton > button:first-child {
+            background: #1565C0;
+            color: white;
+            border: none;
+            padding: 12px 24px;
+            border-radius: 8px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            margin: 10px 2px;
+            cursor: pointer;
+            transition: background-color 0.3s ease;
+            box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.2);
+            font-weight: bold;
+        }
         </style>
     """, unsafe_allow_html=True)
 
@@ -58,39 +98,13 @@ def hide_elements():
 
 def title_and_description():
     st.title("🧕 Alwrity - AI Youtube link to Blog conversion.")
-    with st.expander("**How to Use** Alwrity YT blog Generator? 📝❗"):
-        st.markdown(
-        '''---
-            # How to Download Audio from YouTube Videos and Local Files
-
-			How to download audio from YouTube videos and local files using Python. 
-            Whether you're looking to extract audio for listening on the go or incorporating it into your projects, this guide has got you covered.
-			
-			## Step 1: Accessing YouTube URLs
-			- Check if the provided URL is a YouTube link.
-			- If it is, access the YouTube URL and fetch the highest quality audio stream available.
-			
-			## Step 2: Downloading Audio from YouTube
-			- Filter for the audio-only stream of the YouTube video.
-			- Download the audio file to the specified output path.
-			- Display a progress bar to track the download process.
-			
-			## Step 3: Handling Local Audio Files
-			- If the input is not a YouTube URL, check if it's a valid local audio file.
-			- If it is, proceed with the provided local file path.
-			
-			## Step 4: Checking File Size
-			- Ensure that the downloaded audio file does not exceed the maximum file size limit of 24MB.
-			- If the file size exceeds the limit, log an error message and handle accordingly.
-			
-		---''')
 
 
 def input_section():
     with st.expander("**PRO-TIP** - Better input yield, better results.", expanded=True):
-        yt_url = st.text_input('**To get blog content, Enter Full Youtube Video URL:**',
-                        help="A single YT URL, less than 25 minutes",
-                               placeholder="https://www.youtube.com/watch?v=vQChW_jgCLM")
+        yt_url = st.text_input('**Enter Full Youtube Video URL:**',
+                help="A single YT URL, less than 10 minutes",
+                placeholder="https://www.youtube.com/watch?v=vQChW_jgCLM")
 
         st.write("Contact us, to input long videos(duration of hours), local audio files & other AI solutions for your business needs.")
         if st.button('**Write YT Blog**'):
@@ -105,64 +119,17 @@ def input_section():
             else:
                 st.error("Input Title/Topic of content to outline, Required!")
 
-    page_bottom()
-
 
 def generate_yt_blog(yt_url):
-        """ """
-        audio_file = None
-        yt = YouTube(yt_url)
-        audio_stream = yt.streams.filter(only_audio=True).first()
-
-        if audio_stream is None:
-            st.error("Error: No audio stream found for this video.")
-            return None
-
-        with st.spinner(f"Downloading audio for: {yt.title}"):
-            audio_file = audio_stream.download()
-            if not audio_file:
-                st.error(f"Failed to download audio file: {audio_file}")
-                return None
-
-        # Checking file size
-        max_file_size = 24 * 1024 * 1024  # 24MB
-        file_size = os.path.getsize(audio_file)
-        # Convert file size to MB for logging
-        file_size_MB = file_size / (1024 * 1024)  # Convert bytes to MB
-        st.info(f"Downloaded Audio Size is: {file_size_MB:.2f} MB")
-        if file_size > max_file_size:
-            st.error("Error: File size exceeds 24MB limit.")
-            return None
-
-        client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
-        with st.spinner("Initializing OpenAI client for transcription."):
-            transcript = client.audio.transcriptions.create(
-                model="whisper-1",
-                file=open(audio_file, "rb"),
-                response_format="text"
-            )
-        # Remove the downloaded, now that we have the transcription.
-        if os.path.exists(audio_file):
-            os.remove(audio_file)
-
-        # Summarizing the content of the YouTube video
-        return summarize_youtube_video(transcript)
-        
-#    return transcript, yt.title
-#    return openai_chatgpt(prompt)
-
-
-def page_bottom():
-    """Display the bottom section of the web app."""
-    with st.expander("Alwrity - Content outline generator - powered by AI (OpenAI, Gemini Pro)."):
-        st.write('''
-			
-			[Get Started Now, Get custom AI solutions](https://alwrity.com)
-			---	
-			Youtube URL to Blog generator - powered by AI (OpenAI, Gemini Pro).	
-			Implemented by [Alwrity](https://alwrity.com).
-        ''')
-
+        """Use Gemini Flash to get transcript from youtube URL """
+        with st.spinner(f"Transcribing youtube URL: {yt_url}"):
+            try:
+                transcript = get_youtube_transcript(yt_url)
+                return summarize_youtube_video(transcript)
+            except Exception as err:
+                st.error(f"Exit: Failed to get response from LLM: {err}")
+                exit(1)
+            
 
 def summarize_youtube_video(yt_transcript):
     """Generates a summary of a YouTube video using OpenAI GPT-3 and displays a progress bar. 
@@ -171,8 +138,6 @@ def summarize_youtube_video(yt_transcript):
     Returns:
       A string containing the summary of the video.
     """
-
-    st.info("Start summarize_youtube_video..")
     prompt = f"""
         You are an expert content writer specializing in digital content writing. I will provide you with a transcript. 
         Your task is to transform a given transcript into a well-formatted and informative blog article. 
@@ -194,23 +159,67 @@ def summarize_youtube_video(yt_transcript):
         Follow above guidelines to craft a blog content from the following transcript:
         \n\nTranscript: {yt_transcript}
         """
-    return openai_chatgpt(prompt)
+    
+    try:
+        response = generate_text_with_exception_handling(prompt)
+        return response
+    except Exception as err:
+        st.error(f"Exit: Failed to get response from LLM: {err}")
+        exit(1)
 
 
 @retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(6))
-def openai_chatgpt(prompt, model="gpt-3.5-turbo-0125", max_tokens=2048, top_p=0.6, n=3):
+def generate_text_with_exception_handling(prompt):
+    """
+    Generates text using the Gemini model with exception handling.
+
+    Args:
+        api_key (str): Your Google Generative AI API key.
+        prompt (str): The prompt for text generation.
+
+    Returns:
+        str: The generated text.
+    """
+
     try:
-        client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
-        response = client.chat.completions.create(
-                model=model,
-                messages=[{"role": "user", "content": prompt}],
-                max_tokens=max_tokens,
-                n=n,
-                top_p=top_p
-            )
-        return response.choices[0].message.content
-    except Exception as err:
-        st.error(f"An error occurred: {err}")
+        genai.configure(api_key=os.getenv('GEMINI_API_KEY'))
+
+        generation_config = {
+            "temperature": 0.7,
+            "top_k": 0,
+            "max_output_tokens": 4096,
+        }
+
+        safety_settings = [
+            {
+                "category": "HARM_CATEGORY_HARASSMENT",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+            },
+            {
+                "category": "HARM_CATEGORY_HATE_SPEECH",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+            },
+            {
+                "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+            },
+            {
+                "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+            },
+        ]
+
+        model = genai.GenerativeModel(model_name="gemini-1.5-flash",
+                                      generation_config=generation_config,
+                                      safety_settings=safety_settings)
+
+        convo = model.start_chat(history=[])
+        convo.send_message(prompt)
+        return convo.last.text
+
+    except Exception as e:
+        st.exception(f"An unexpected error occurred: {e}")
+        return None
 
 
 if __name__ == "__main__":
